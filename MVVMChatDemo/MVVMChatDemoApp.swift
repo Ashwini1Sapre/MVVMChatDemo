40378059f104c18e62da224e0e65b6e023c305b3
ashwinisapreknoxpo <ashwini.sapre@knoxpo.com>
2021-04-17 10:54:26 +0530

    Initial Commit

---
+++ MVVMChatDemo/MVVMChatDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMChatDemoApp.swift
+//  MVVMChatDemo
+//
+//  Created by Knoxpo MacBook Pro on 17/04/21.
+//
+
+import SwiftUI
+
+@main
+struct MVVMChatDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
